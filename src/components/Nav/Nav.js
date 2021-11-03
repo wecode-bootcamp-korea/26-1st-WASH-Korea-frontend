0a1d09abca7c2267bd00f7dfded3ff217c289702
standup8 <borapark13@gmail.com>
2021-11-03 18:35:30 +0900

    WIP : Nav 공통 컴포넌트 수정중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../Nav/Nav.scss';
 import { HiOutlineSearch } from 'react-icons/hi';
 import { HiShoppingCart } from 'react-icons/hi';
 import { FaUserCircle } from 'react-icons/fa';
+import '../Nav/Nav.scss';
+
+const MENU = ['제품', '워시 소개', '매장 안내', '이벤트'];
+const SUB_MENU = [
+  ['샤워', '샴푸', '바디워시', '비누', '배쓰 밤', '샤워용품'],
+  ['바디', '바디 로션', '바디 미스트', '바디 오일', '바디 스크럽'],
+  ['페이스', '폼클렌징', '팩', '스킨', '로션'],
+  ['헤어', '헤어 에센스', '헤어 미스트', '헤어 스프레이', '드라이 샴푸'],
+];
 
 export class Nav extends Component {
   constructor(props) {
@@ -13,42 +21,39 @@ export class Nav extends Component {
     };
   }
 
-  onMouseEnter = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  onMouseLeave = () => {
-    this.setState({
-      visible: false,
-    });
+  onHover = visible => {
+    visible
+      ? this.setState({ visible: true })
+      : this.setState({ visible: false });
   };
 
   render() {
     const { visible } = this.state;
+
     return (
-      <nav onMouseLeave={this.onMouseLeave}>
+      <nav
+        onMouseLeave={() => {
+          this.onHover(false);
+        }}
+      >
         <div className="innerNav">
           <h1 className="logo">
             <Link to="/">
               <img src="/images/logo.png" alt="wash korea logo" />
             </Link>
           </h1>
-          <div className="gnb" onMouseEnter={this.onMouseEnter}>
+          <div
+            className="gnb"
+            onMouseEnter={() => {
+              this.onHover(true);
+            }}
+          >
             <ul className="depth01">
-              <li>
-                <Link to="/">제품</Link>
-              </li>
-              <li>
-                <Link to="/">워시 소개</Link>
-              </li>
-              <li>
-                <Link to="/">매장 안내</Link>
-              </li>
-              <li>
-                <Link to="/">이벤트</Link>
-              </li>
+              {MENU.map((value, idx) => (
+                <li key={idx}>
+                  <Link to="/">{value}</Link>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="users">
@@ -66,77 +71,9 @@ export class Nav extends Component {
         {visible && (
           <div className="lnbWrapper">
             <div className="lnb">
-              <ul className="depth02">
-                <li className="subTitle">
-                  <Link to="/">샤워</Link>
-                </li>
-                <li>
-                  <Link to="/">샴푸</Link>
-                </li>
-                <li>
-                  <Link to="/">바디워시</Link>
-                </li>
-                <li>
-                  <Link to="/">비누</Link>
-                </li>
-                <li>
-                  <Link to="/">배쓰 밤</Link>
-                </li>
-                <li>
-                  <Link to="/">샤워용품</Link>
-                </li>
-              </ul>
-              <ul className="depth02">
-                <li className="subTitle">
-                  <Link to="/"> 바디</Link>
-                </li>
-                <li>
-                  <Link to="/">바디 로션</Link>
-                </li>
-                <li>
-                  <Link to="/">바디 미스트</Link>
-                </li>
-                <li>
-                  <Link to="/">바디 오일</Link>
-                </li>
-                <li>
-                  <Link to="/">바디 스크럽</Link>
-                </li>
-              </ul>
-              <ul className="depth02">
-                <li className="subTitle">
-                  <Link to="/">페이스</Link>
-                </li>
-                <li>
-                  <Link to="/">폼클렌징</Link>
-                </li>
-                <li>
-                  <Link to="/">팩</Link>
-                </li>
-                <li>
-                  <Link to="/">스킨</Link>
-                </li>
-                <li>
-                  <Link to="/">로션</Link>
-                </li>
-              </ul>
-              <ul className="depth02">
-                <li className="subTitle">
-                  <Link to="/">헤어</Link>
-                </li>
-                <li>
-                  <Link to="/">헤어 에센스</Link>
-                </li>
-                <li>
-                  <Link to="/">헤어 미스트</Link>
-                </li>
-                <li>
-                  <Link to="/">헤어 스프레이</Link>
-                </li>
-                <li>
-                  <Link to="/">드라이 샴푸</Link>
-                </li>
-              </ul>
+              {SUB_MENU.map((value, idx) => {
+                return <SubMenu sub={value[idx]} i={idx} />;
+              })}
             </div>
           </div>
         )}
@@ -146,3 +83,19 @@ export class Nav extends Component {
 }
 
 export default Nav;
+
+class SubMenu extends Component {
+  render() {
+    const { sub } = this.props;
+    console.log(sub);
+    return (
+      <div className="depth02">
+        {/* {this.props.map((value, idx) => {
+          <li>
+            <Link to="/">{value}</Link>
+          </li>;
+        })} */}
+      </div>
+    );
+  }
+}
