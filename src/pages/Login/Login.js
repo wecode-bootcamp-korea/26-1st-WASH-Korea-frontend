b1b7c2acbd12b222a261f5e425d8240e8d95d03d
sugyinbrs <sgnglee@gmail.com>
2021-11-09 17:17:36 +0900

    Add: Login 레이아웃 진행 중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import './Login.scss';
 
 export class Login extends Component {
   render() {
-    return <div>Login입니다.</div>;
+    return (
+      <main className="login">
+        <div className="loginBox">
+          <div className="title">로그인</div>
+          <ul className="memberCheck">
+            <li className="member">회원</li>
+            <li className="nonMember">비회원</li>
+          </ul>
+          <div className="inputBox">
+            <form className="input">
+              <input className="inputId" type="text" placeholder="아이디" />
+              <input
+                className="inputPw"
+                type="password"
+                placeholder="비밀번호"
+              />
+            </form>
+          </div>
+          <div className="checkBox">
+            <input className="saveId" type="checkbox" />
+            <label for="saveId">아이디 저장</label>
+          </div>
+        </div>
+      </main>
+    );
   }
 }
 
