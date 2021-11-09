7c15d28aa81918a6f7640784c01e1b9671e09df1
sugyinbrs <sgnglee@gmail.com>
2021-11-10 01:00:07 +0900

    Modified: 로그인 인증 인가 토큰 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -32,6 +32,7 @@ export class Login extends Component {
         if (result.message === 'USER_DOES_NOT_EXISTS') {
           alert('아이디 혹은 비밀번호를 다시 확인해주세요.');
         } else {
+          localStorage.setItem('token', result.token);
           this.goToMain();
         }
       });
