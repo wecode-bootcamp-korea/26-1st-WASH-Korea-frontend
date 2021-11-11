ed79f3d6fba8769302b3bb3724afd0860da0b0ba
yedio <yediobjet@gmail.com>
2021-11-11 18:31:49 +0900

    Modify : config.js Routes.js 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
-
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductList from './pages/ProductList/ProductList';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Footer from './components/Footer/Footer';
 import Cart from './pages/Cart/Cart';
 
 class Routes extends Component {
@@ -15,10 +14,11 @@ class Routes extends Component {
       <Router>
         <Nav />
         <Switch>
+          <Route exact path="/" component={ProductList} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/productDetail" component={ProductDetail} />
-          <Route exact path="/productList" component={ProductList} />
+          <Route exact path="/productdetail/:id" component={ProductDetail} />
+          <Route exact path="/productlist" component={ProductList} />
           <Route exact path="/cart" component={Cart} />
         </Switch>
         <Footer />
